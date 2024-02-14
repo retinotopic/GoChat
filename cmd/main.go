c8e5572f0dbd8ab08c0202971909c4d04b384756
retinotopic <retinotopic@proton.me>
2024-02-14 23:55:50 +0300

    Splitting interfaces doesn't really fit whose methods are used entirely by a single map, so created a new type map[string]Provider with http.handler methods

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/retinotopic/GoChat/internal/auth"
-	"github.com/retinotopic/GoChat/internal/providers/gfirebase"
-	"github.com/retinotopic/GoChat/internal/providers/google"
 	server "github.com/retinotopic/GoChat/internal/routes"
 )
 
 func main() {
 	addr := flag.String("addr", "localhost:8080", "address to listen on")
-	auth.CurrentProviders = auth.Providers{
-		"google":    google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), "http://localhost:8080/google/CompleteAuth"),
-		"gfirebase": gfirebase.New(os.Getenv("WEB_API_KEY"), os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"), "http://localhost:8080/gfirebase/CompleteAuth"),
-	}
+
 	flag.Parse()
 	fmt.Println(*addr)
 	srv := server.NewServer(*addr)
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,8 +2,11 @@ package server
 
 import (
 	"net/http"
+	"os"
 
-	"github.com/retinotopic/GoChat/internal/auth"
+	session "github.com/retinotopic/GoChat/internal/auth"
+	"github.com/retinotopic/GoChat/internal/providers/gfirebase"
+	"github.com/retinotopic/GoChat/internal/providers/google"
 )
 
 type Server struct {
@@ -14,18 +17,15 @@ func NewServer(addr string) *Server {
 	return &Server{addr: addr}
 }
 func (s *Server) Run() error {
+	CurrentProviders := session.Session{
+		"google":    google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), "http://localhost:8080/google/CompleteAuth"),
+		"gfirebase": gfirebase.New(os.Getenv("WEB_API_KEY"), os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"), "http://localhost:8080/gfirebase/CompleteAuth"),
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{provider}/BeginLoginCreate", func(w http.ResponseWriter, r *http.Request) {
-		auth.RefreshI(auth.CurrentProviders[r.PathValue("provider")]).ServeHTTP(w, r)
-	})
-	mux.HandleFunc("/{provider}/CompleteLoginCreate", func(w http.ResponseWriter, r *http.Request) {
-		auth.CompleteLoginCreateI(auth.CurrentProviders[r.PathValue("provider")]).ServeHTTP(w, r)
-	})
-	mux.HandleFunc("/refresh/{provider}", func(w http.ResponseWriter, r *http.Request) {
-		auth.RefreshI(auth.CurrentProviders[r.PathValue("provider")]).ServeHTTP(w, r)
-	})
-	mux.HandleFunc("/refresh/revoke/{provider}", func(w http.ResponseWriter, r *http.Request) {
-		auth.RevokeI(auth.CurrentProviders[r.PathValue("provider")]).ServeHTTP(w, r)
-	})
+	mux.HandleFunc("/{provider}/BeginLoginCreate", CurrentProviders.BeginLoginCreate)
+	mux.HandleFunc("/{provider}/CompleteLoginCreate", CurrentProviders.CompleteLoginCreate)
+	mux.HandleFunc("/refresh/{provider}", CurrentProviders.Refresh)
+	mux.HandleFunc("/refresh/revoke/{provider}", CurrentProviders.Revoke)
 	return http.ListenAndServe(s.addr, mux)
 }
