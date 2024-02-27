622f8482211d53d9000f04f74bffafe85c2717fb
retinotopic <retinotopic@proton.me>
2024-02-27 23:15:17 +0300

    I still don't know how to make the database optimally for both message rooms and private messages, only queries have been added as working prototypes

--- internal/db/db.go
+++ internal/db/db.go
@@ -98,15 +98,52 @@ INSERT INTO rooms DEFAULT VALUES
 /
 INSERT INTO users (subject,name) VALUES ('dhdf982hfireb','aboba')
 /
-INSERT INTO messages (payload,room_id,user_id,timestamp) VALUES ('hi aboba',1,1)
+INSERT INTO messages (payload,user_id,room_id) VALUES ('hi arolfix',6,14)
 /
 INSERT INTO room_users_info (room_id,user_id,unread) VALUES (3,2,2)
 /
-SELECT payload,room_id,user_id FROM messages WHERE room_id IN (SELECT room_id FROM messages WHERE user_id=1) ORDER BY room_id,timestamp
+SELECT payload,user_id,room_id FROM messages WHERE room_id IN (SELECT room_id FROM room_users_info WHERE user_id=6) ORDER BY room_id,timestamp
 /
 SELECT user_id,name FROM users WHERE name LIKE '%%';
 /
 CREATE SEQUENCE room_ids START 1;
 /
 INSERT INTO rooms (room_id) VALUES (nextval('room_ids')) RETURNING room_id
+/
+rooms_room_id_seq
+/
+BEGIN;
+WITH roomval AS (INSERT INTO rooms DEFAULT VALUES RETURNING room_id)
+INSERT INTO room_users_info (user_id, room_id, unread)
+SELECT 1, room_id, 0 FROM roomval
+UNION ALL
+SELECT 5, room_id, 0 FROM roomval;
+COMMIT;
+//
+SELECT payload,user_id FROM messages WHERE ((user_id = 6 OR to_user_id = 6) AND room_id IS NULL) ORDER BY timestamp
+//
+SELECT payload, user_id, room_id
+FROM (
+    SELECT payload, user_id, NULL AS room_id, timestamp
+    FROM messages
+    WHERE (user_id = 6 OR to_user_id = 6) AND room_id IS NULL
+    UNION ALL
+    SELECT m.payload, m.user_id, m.room_id, m.timestamp
+    FROM messages m
+    JOIN (
+        SELECT room_id
+        FROM room_users_info
+        WHERE user_id = 6
+    ) ru ON m.room_id = ru.room_id
+)
+ORDER BY COALESCE(room_id, user_id), timestamp;
+//
+SELECT m.payload, m.user_id, m.room_id
+FROM messages m
+JOIN (
+	SELECT room_id
+	FROM room_users_info
+	WHERE user_id = 6
+) ru ON m.room_id = ru.room_id
+ORDER BY m.room_id,m.timestamp
 */
