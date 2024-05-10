941c95781ff1eb175b62fff5f3cda6d6f45a82ad
retinotopic <retinotopic@proton.me>
2024-05-10 23:18:12 +0300

    decoupling db class

--- internal/db/db.go
+++ internal/db/db.go
@@ -78,45 +78,6 @@ func NewClient(sub string, pool *pgxpool.Pool) (*PostgresClient, error) {
 	}
 	return pc, nil
 }
-func (c *PostgresClient) TxManage(flowjson *FlowJSON) {
-	flowjson.Status = "OK"
-	fn, ok := c.funcmap[flowjson.Mode]
-	if !ok {
-		return
-	}
-	if flowjson.SenderOnly {
-		fn(flowjson)
-		return
-	}
-	c.txBegin(flowjson)
-	fn(flowjson)
-	c.txCommit(flowjson)
-	if flowjson.Err != nil {
-		flowjson.Status = flowjson.Err.Error()
-	}
-	c.Chan <- *flowjson
-}
-func (c *PostgresClient) txBegin(flowjson *FlowJSON) {
-	flowjson.Tx, flowjson.Err = flowjson.Tx.Begin(context.Background())
-}
-func (c *PostgresClient) txCommit(flowjson *FlowJSON) {
-	if flowjson.Err == nil {
-		flowjson.Err = flowjson.Tx.Commit(context.Background())
-		if flowjson.Err != nil {
-			flowjson.Status = "internal database error"
-			flowjson.Err = flowjson.Tx.Rollback(context.Background())
-			if flowjson.Err != nil {
-				log.Println("ATTENTION Error rolling back transaction:", flowjson.Err)
-			}
-		}
-	} else {
-		flowjson.Err = flowjson.Tx.Rollback(context.Background())
-		if flowjson.Err != nil {
-			log.Println("ATTENTION Error rolling back transaction:", flowjson.Err)
-			flowjson.Status = "internal database error"
-		}
-	}
-}
 
 // transaction insert messages plus increment unread messages in room_users table
 func (c *PostgresClient) SendMessage(flowjson *FlowJSON) {
