d759bf33668617dfe2aa66f523c9aa3e4ae25fd1
retinotopic <retinotopic@proton.me>
2024-09-28 03:33:26 +0300

    implemented redis pubsuber

--- internal/models/flowjson.go
+++ internal/models/flowjson.go
@@ -2,9 +2,10 @@ package models
 
 type Event struct {
 	Event        string `json:"Event"`
+	Subscription bool   // publish with subscription or not
+	Unsubscribe  bool   // subscribe or unsubsribe
+	Publish      string // publish channel
 	UserId       uint32 `json:"UserId"`
-	Subscription string
-	PubSub       uint32
 	ErrorMsg     string `json:"ErrorMsg"`
-	Data         []byte `json:"Data" `
+	Data         []byte `json:"-"`
 }
