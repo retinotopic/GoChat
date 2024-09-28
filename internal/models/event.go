0857e7639e16fb544d566b4a369da14256eb1d9c
retinotopic <retinotopic@proton.me>
2024-09-28 03:35:22 +0300

    model's file renamed

--- internal/models/event.go
+++ internal/models/event.go
@@ -3,7 +3,7 @@ package models
 type Event struct {
 	Event        string `json:"Event"`
 	Subscription bool   // publish with subscription or not
-	Unsubscribe  bool   // subscribe or unsubsribe
+	Unsubscribe  bool   // subscribe or unsubscribe
 	Publish      string // publish channel
 	UserId       uint32 `json:"UserId"`
 	ErrorMsg     string `json:"ErrorMsg"`
